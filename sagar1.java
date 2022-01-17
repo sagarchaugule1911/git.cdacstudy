f50006ec60e30e4cdfa50e826c2b8c1243f4b39f
Sagar Vitthal Chaugule <sagarchaugule1911@gmail.com>
2022-01-17 15:08:57 +0530

    final commit

---
+++ sagar1.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        int d = 15 / (10 /2) ;
+        System.out.println(d);
+        System.out.println("I am newwwwwwwwwwwww");
+    }
+}
